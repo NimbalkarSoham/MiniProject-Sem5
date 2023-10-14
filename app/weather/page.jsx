33b8bb2aec7aaa37e3ef922471b6d5179f0f96af
Niyati2609 <niyatig26@gmail.com>
2023-10-14 09:38:23 +0530

    weather

--- app/weather/page.jsx
+++ app/weather/page.jsx
@@ -1,15 +1,60 @@
-"use client"
-import { data } from 'autoprefixer';
-import { useSession } from 'next-auth/react'
-import React from 'react'
+"use client" 
+import axios from 'axios';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import Weather from '@components/Weather';
+import Spinner from '@components/Spinner';
+export default function Home() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
 
-const Weather = () => {
-    
-    const {data: session} = useSession();
-    console.log()
-  return (
-    <div>Weather at {JSON.stringify(session)}</div>
-  )
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+      // console.log(response.data);
+    });
+    setCity('');
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div>
+        
+        {/* Overlay */}
+        <div className='absolute top-100 left-0 right-0 bottom-0  ' />
+        {/* Background image */}
+        
+        {/* Search */}
+        <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
+          <form
+            onSubmit={fetchWeather}
+            className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'
+          >
+            <div>
+              <input
+                onChange={(e) => setCity(e.target.value)}
+                className='bg-transparent border-none text-black focus:outline-none text-2xl'
+                type='text'
+                placeholder='Search city'
+              />
+            </div>
+            <button onClick={fetchWeather}>
+              <BsSearch size={20} />
+            </button>
+          </form>
+        </div>
+
+        {/* Weather */}
+        {weather.main && <Weather data={weather} />}
+      </div>
+    );
+  }
 }
-
-export default Weather
