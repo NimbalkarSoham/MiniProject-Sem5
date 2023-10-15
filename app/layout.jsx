43a2841a9c102f27079d349c5cb0081167cc0aad
Niyati2609 <niyatig26@gmail.com>
2023-10-15 12:55:47 +0530

    Weather updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,19 +10,25 @@ export const metadata = {
 
 const RootLayout = ({ children }) => {
 
-    
-  return (
-    <html lang='en'>
-        <body>
-            <Provider>
-                <main className="app">
-                    <Nav/>
-                    {children}
-                </main>
-            </Provider>
-        </body>
-    </html>
-  )
+
+    return (
+        <html lang='en'>
+            <body>
+                <Provider>
+                    <main className="app">
+                        <Nav />
+                        {children}
+                        <footer className='mt-96 green1  text-white width-[100] p-4 text-center text-xl'>
+                            <p>Contact Us:</p>
+                            <p>Email: agrifarm@example.com</p>
+                            <p>Phone: (123) 456-7890</p>
+                            <p>Address: 123 Main St, City, Country</p>
+                        </footer>
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
 }
 
 export default RootLayout
