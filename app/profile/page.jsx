2af08f2702a8306925b2d52d3032b838df643ca0
Soham Nimbalkar <soham.nimbalkar08@gmail.com>
2024-02-18 22:30:01 +0530

    resolving build issues

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,33 +1,27 @@
-"use client"
-import React from 'react'
-import {useState,useEffect} from 'react'
-import {useSession} from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import ProfileFeed from '@components/ProfileFeed'
-
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import ProfileFeed from "@components/ProfileFeed";
 
 const MyProfile = () => {
-    const router = useRouter();
-    const {data:session}=useSession()
-    const[posts,setPosts]=useState([])
-    const user = JSON.parse(localStorage.getItem('user'));
-    useEffect(()=>{
-        const fetchPosts=async()=>{
-            const response=await fetch(`/api/users/${session?.user.id}/posts`)
-            const data=await response.json()
-            setPosts(data)
-         }
-        if(session?.user.id)fetchPosts()
-    },[session?.user.id])
-  const handleEdit=()=>{
-
-  } 
-  const handleDelete=async()=>{
-
-  } 
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  //const user = JSON.parse(localStorage.getItem('user'));
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
   return (
     <>
-      
       <ProfileFeed
         name={session?.user.name}
         desc="welcome to your personalized Profile page"
@@ -36,8 +30,7 @@ const MyProfile = () => {
         handleDelete={handleDelete}
       />
     </>
-    
-  )
-}
+  );
+};
 
-export default MyProfile
+export default MyProfile;
